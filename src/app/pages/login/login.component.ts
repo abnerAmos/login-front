9af09274b0ee25470cc7dfe2cd7b9ab11a1f4fbd
abnerAmos <abner.fsouza1@gmail.com>
2025-02-18 09:22:44 -0300

    Concluindo estilização de botões, ajuste nas imagens e inicio da construção dos inputs dentro do formulário

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
 import { LoginComponent } from '../../components/login/login.component';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login-page',
-  imports: [LoginComponent],
+  imports: [
+    LoginComponent,
+    ReactiveFormsModule
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginPage {
+  loginForm!: FormGroup;
 
+  constructor() {
+    this.loginForm = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required, Validators.minLength(6)])
+    })
+  }
 }
