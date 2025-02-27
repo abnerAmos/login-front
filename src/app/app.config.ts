ab5e76f0f4fdc993c647495885cc7faa9fd16a54
abnerAmos <abner.fsouza1@gmail.com>
2025-02-27 19:03:19 -0300

    Documentação do app.config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,10 +5,36 @@ import { routes } from './app.routes';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
+/**
+ * Configuração global da aplicação Angular.
+ * Define os provedores principais usados pelo framework, como roteamento,
+ * manipulação de mudanças de zona, cliente HTTP e animações assíncronas.
+ */
 export const appConfig: ApplicationConfig = {
   providers: [
+    /**
+     * Ativa otimizações na detecção de mudanças do Angular.
+     * - `eventCoalescing: true`: Agrupa eventos assíncronos para reduzir
+     *    verificações desnecessárias na detecção de mudanças.
+     */
     provideZoneChangeDetection({ eventCoalescing: true }),
+
+    /**
+     * Fornece o serviço de roteamento da aplicação, utilizando as rotas
+     * definidas no arquivo `app.routes.ts`.
+     */
     provideRouter(routes),
-    provideHttpClient(withFetch()), provideAnimationsAsync()
+
+    /**
+     * Configura o cliente HTTP da aplicação para realizar requisições HTTP.
+     * - `withFetch()`: Habilita o uso da API Fetch nativa para requisições HTTP,
+     *    melhorando o desempenho em navegadores modernos.
+     */
+    provideHttpClient(withFetch()),
+
+    /**
+     * Ativa o suporte para animações assíncronas, melhorando o desempenho da aplicação.
+     */
+    provideAnimationsAsync()
   ]
 };
