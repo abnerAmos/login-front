266c247788126e5f7a7a8c96e1138cba38955d86
abnerAmos <abner.fsouza1@gmail.com>
2025-02-28 15:01:08 -0300

    Documentação das apis

--- src/app/services/api/login.service.ts
+++ src/app/services/api/login.service.ts
@@ -4,20 +4,50 @@ import { LoginResponse } from '../../types/login-response.type';
 import { tap } from 'rxjs';
 import { TokenService } from '../token/token.service';
 
+/**
+ * Serviço responsável por gerenciar autenticação e registro de usuários.
+ * Ele se comunica com a API para realizar login e cadastro.
+ */
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root' // Torna o serviço disponível em toda a aplicação sem necessidade de importação manual.
 })
 export class LoginService {
+  /**
+   * URL base da API de autenticação.
+   */
   private apiUrl: string = "http://localhost:8080/auth";
 
+  /**
+   * Construtor do serviço.
+   * @param httpClient Serviço para realizar chamadas HTTP.
+   * @param tokenService Serviço para gerenciar tokens de autenticação.
+   */
   constructor(private httpClient: HttpClient, private tokenService: TokenService) { }
 
+  /**
+   * Realiza o login do usuário.
+   * - Envia uma requisição POST para a API com email e senha.
+   * - Em caso de sucesso, armazena os tokens de acesso e refresh.
+   * 
+   * @param email Email do usuário.
+   * @param password Senha do usuário.
+   * @returns Um Observable contendo a resposta do login.
+   */
   login(email: string, password: string) {
     return this.httpClient.post<LoginResponse>(`${this.apiUrl}/login`, { email, password }).pipe(
       tap(response => this.tokenService.saveTokens(response.token, response.refreshToken))
     );
   }
 
+  /**
+   * Realiza o cadastro de um novo usuário.
+   * - Envia uma requisição POST para a API com os dados do usuário.
+   * 
+   * @param username Nome do usuário.
+   * @param email Email do usuário.
+   * @param password Senha do usuário.
+   * @returns Um Observable contendo a resposta do cadastro.
+   */
   register(username: string, email: string, password: string) {
     return this.httpClient.post(`${this.apiUrl}/register`, { username, email, password })
   }
